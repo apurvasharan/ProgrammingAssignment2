f0f433a7d069562641d412427fd9fe91465c8013
Apurva Sharan <apurvash@gmail.com>
2015-11-22 23:16:56 +0530

    Completed Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This module implements a cached version matrix object.
+## Currently it implements matrix inversion operation through 
+## R's builtin solve ().
 
-## Write a short comment describing this function
-
+## Returns a closure which caches computed values for matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    solved <- NULL
+    set <- function (y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function () x
+    getsolved <- function () solved
+    setsolved <- function (nsolved) {
+        solved <<- nsolved
+    }
+    list (set = set, get = get, getsolved = getsolved, setsolved = setsolved)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Invoke solve () operation on the cached matrix. 
+## This computes inverse of matix if no 2nd argument is provided, 
+## else the 2nd argument is used as the RHS of the linear system
+## of equations for finding the solution
+cacheSolve <- function(cm, ...) {
+    tryCatch({
+                inv <- cm$getsolved()
+                if (!is.null (inv)) {
+                    message ("getting from cache")
+                    return (inv)
+                }
+            },
+            error = function (e) {
+                stop ("E: Provide a cached matrix returned by makeCachedMatrix ()")
+            },
+            warning = function(w) {
+                stop ("W: Provide a cached matrix returned by makeCachedMatrix ()")
+            }
+    )
+    inv <- tryCatch ({
+                        message ("computing from scratch")
+                        mat <- cm$get()
+                        solve(mat, ...)
+                     },
+                     error = function(e) {
+                         message ("Error during matrix solve operation")
+                         print (e)
+                         NULL
+                     },
+                     warning = function (w) {
+                         message ("Warning during matrix solve operation")
+                         print (w)
+                         NULL
+                     })
+    cm$setsolved(inv)
+    return (inv)
 }
